df4dd7018195c16b368ef8c8a60988ae4a9e6ca1
Adam Chalmers <adam.chalmers@zoo.dev>
2025-08-21 13:08:37 -0500

    Notes from Nick's review
    - Fix a typo (it's euclidean, not euclidian)
    - Add a linter to show users problems with their constraints
    - Slightly optimize faer in debug builds for faster tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@ mod id;
 /// Numeric solver using sparse matrices.
 mod solver;
 
+const EPSILON: f64 = 0.00000001;
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("{0}")]
@@ -32,12 +34,25 @@ pub enum NonLinearSystemError {
 pub struct SolveOutcome {
     pub final_values: Vec<f64>,
     pub iterations: usize,
+    pub lints: Vec<Lint>,
+}
+
+#[derive(Debug)]
+pub struct Lint {
+    pub about_constraint: Option<usize>,
+    pub content: String,
 }
 
 /// Given some initial guesses, constrain them.
 /// Returns the same variables in the same order, but constrained.
-pub fn solve(constraints: Vec<Constraint>, initial_guesses: Vec<(Id, f64)>) -> Result<SolveOutcome, Error> {
-    let (all_variables, mut final_values): (Vec<Id>, Vec<f64>) = initial_guesses.into_iter().unzip();
+pub fn solve(
+    constraints: Vec<Constraint>,
+    initial_guesses: Vec<(Id, f64)>,
+) -> Result<SolveOutcome, Error> {
+    let (all_variables, mut final_values): (Vec<Id>, Vec<f64>) =
+        initial_guesses.into_iter().unzip();
+    let lints = lint(&constraints);
+
     let mut model = Model::new(constraints, all_variables);
     let iterations = newton_faer::solve(
         &mut model,
@@ -45,10 +60,50 @@ pub fn solve(constraints: Vec<Constraint>, initial_guesses: Vec<(Id, f64)>) -> R
         newton_faer::NewtonCfg::sparse().with_adaptive(true),
     )
     .map_err(|errs| Error::Solver(Box::new(errs.into_error())))?;
+
     Ok(SolveOutcome {
         final_values,
         iterations,
+        lints,
     })
+}
+
+fn nearly_eq(a: f64, b: f64) -> bool {
+    (a - b).abs() < EPSILON
+}
+
+fn lint(constraints: &[Constraint]) -> Vec<Lint> {
+    let mut lints = Vec::default();
+    for (i, constraint) in constraints.iter().enumerate() {
+        match constraint {
+            Constraint::LinesAtAngle(_, _, constraints::AngleKind::Other(theta))
+                if nearly_eq(theta.to_degrees(), 0.0)
+                    || nearly_eq(theta.to_degrees(), 360.0)
+                    || nearly_eq(theta.to_degrees(), 180.0) =>
+            {
+                lints.push(Lint {
+                    about_constraint: Some(i),
+                    content: format!(
+                        "Suggest using AngleKind::Parallel instead of AngleKind::Other({})",
+                        theta.to_degrees()
+                    ),
+                });
+            }
+            Constraint::LinesAtAngle(_, _, constraints::AngleKind::Other(theta))
+                if nearly_eq(theta.to_degrees(), 90.0) || nearly_eq(theta.to_degrees(), -90.0) =>
+            {
+                lints.push(Lint {
+                    about_constraint: Some(i),
+                    content: format!(
+                        "Suggest using AngleKind::Perpendicular instead of AngleKind::Other({})",
+                        theta.to_degrees()
+                    ),
+                });
+            }
+            _ => {}
+        }
+    }
+    lints
 }
 
 #[cfg(test)]
@@ -322,7 +377,9 @@ mod tests {
             .join("\n");
         let all_labels = points
             .iter()
-            .map(|((x, y), label)| format!("set label \"{label} ({x}, {y})\" at {x},{y} offset 1,1"))
+            .map(|((x, y), label)| {
+                format!("set label \"{label} ({x}, {y})\" at {x},{y} offset 1,1")
+            })
             .collect::<Vec<_>>()
             .join("\n");
         let components = points
