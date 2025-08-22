6f0b3e1ec2d9ef8009810448e8d1b6da73a64f09
Adam Chalmers <adam.chalmers@zoo.dev>
2025-08-21 21:48:44 -0500

    Benchmark the solver in wasm (#5)
    To build the WASM library, run
    
    ```
    wasm-pack build --target web
    ```
    
    then open `index.html` (make sure to serve it from localhost)
    
    This updates the ezpz playground to run the "two dependent rectangles" benchmark 100 times and measure the runtime. It clocks in at 315us in Chrome via WASM, and 83us on my Macbook. That means WASM is only 3.75x slower than native code for this -- I think that's actually pretty incredible. 3,174 solves per second means is well above our 60 solves per second goal.
    
    This also proves you can compile and run ezpz in the browser, which is great as Faer doesn't officially advertise that compatibility as a feature.

--- ezpz-wasm/src/lib.rs
+++ ezpz-wasm/src/lib.rs
@@ -1,6 +1,92 @@
+use kcl_ezpz::{
+    Constraint, IdGenerator,
+    datatypes::{DatumPoint, LineSegment},
+    solve,
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn hello() -> i32 {
     33
+}
+
+#[wasm_bindgen]
+pub fn test_faer() -> f64 {
+    use faer::mat;
+
+    let a = mat![
+        [1.0, 5.0, 9.0], //
+        [2.0, 6.0, 10.0],
+        [3.0, 7.0, 11.0],
+        [4.0, 8.0, 12.0f64],
+    ];
+
+    a[(0, 0)]
+}
+
+#[wasm_bindgen]
+pub fn benchmark() -> Vec<f64> {
+    let mut id_generator = IdGenerator::default();
+    let p0 = DatumPoint::new(&mut id_generator);
+    let p1 = DatumPoint::new(&mut id_generator);
+    let p2 = DatumPoint::new(&mut id_generator);
+    let p3 = DatumPoint::new(&mut id_generator);
+    let line0_bottom = LineSegment::new(p0, p1, &mut id_generator);
+    let line0_right = LineSegment::new(p1, p2, &mut id_generator);
+    let line0_top = LineSegment::new(p2, p3, &mut id_generator);
+    let line0_left = LineSegment::new(p3, p0, &mut id_generator);
+    // Second square (upper case IDs)
+    let p5 = DatumPoint::new(&mut id_generator);
+    let p6 = DatumPoint::new(&mut id_generator);
+    let p7 = DatumPoint::new(&mut id_generator);
+    let line1_bottom = LineSegment::new(p2, p5, &mut id_generator);
+    let line1_right = LineSegment::new(p5, p6, &mut id_generator);
+    let line1_top = LineSegment::new(p6, p7, &mut id_generator);
+    let line1_left = LineSegment::new(p7, p2, &mut id_generator);
+    // First square (lower case IDs)
+    let constraints0 = vec![
+        Constraint::Fixed(p0.id_x(), 1.0),
+        Constraint::Fixed(p0.id_y(), 1.0),
+        Constraint::Horizontal(line0_bottom),
+        Constraint::Horizontal(line0_top),
+        Constraint::Vertical(line0_left),
+        Constraint::Vertical(line0_right),
+        Constraint::Distance(p0, p1, 4.0),
+        Constraint::Distance(p0, p3, 3.0),
+    ];
+
+    // Start p at the origin, and q at (1,9)
+    let initial_guesses = vec![
+        // First square.
+        (p0.id_x(), 1.0),
+        (p0.id_y(), 1.0),
+        (p1.id_x(), 4.5),
+        (p1.id_y(), 1.5),
+        (p2.id_x(), 4.0),
+        (p2.id_y(), 3.5),
+        (p3.id_x(), 1.5),
+        (p3.id_y(), 3.0),
+        // Second square.
+        (p5.id_x(), 5.5),
+        (p5.id_y(), 3.5),
+        (p6.id_x(), 5.0),
+        (p6.id_y(), 4.5),
+        (p7.id_x(), 2.5),
+        (p7.id_y(), 4.0),
+    ];
+
+    let constraints1 = vec![
+        Constraint::Horizontal(line1_bottom),
+        Constraint::Horizontal(line1_top),
+        Constraint::Vertical(line1_left),
+        Constraint::Vertical(line1_right),
+        Constraint::Distance(p2, p5, 4.0),
+        Constraint::Distance(p2, p7, 4.0),
+    ];
+
+    let mut constraints = constraints0;
+    constraints.extend(constraints1);
+    let actual = solve(constraints.clone(), initial_guesses.clone()).unwrap();
+    actual.final_values
+    // vec![]
 }
