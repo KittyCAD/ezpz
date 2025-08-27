e5b9fb960337a51437922f5b6348047f65f4159b
Adam Chalmers <adam.chalmers@zoo.dev>
2025-08-27 23:38:52 +1200

    Don't fill in unnecessary zeroes (#30)
    Faer is a sparse matrix solver. Its website has good docs, which I didn't read when I started this project, because I only read the (very minimal) docs of newton_faer. The website explains how to properly create [sparse matrices](https://faer.veganb.tw/docs/sparse-linalg/creating-matrix/).
    
    When I read it today, I realized that I was filling in _every single zero in the sparse matrix_, essentially transforming it into a dense matrix. You will not be surprised to learn that the sparse matrix algorithms are dramatically slower when operating on dense matrices.
    
    This PR achieves a 40x speedup. As they say, "if you make it 40% faster, you did something very smart. If you made it 40x faster, you stopped doing something stupid."
    
    <img width="1728" height="634" alt="dense to sparse" src="https://github.com/user-attachments/assets/43439151-a3e8-424d-85d1-b400be37cd1e" />

--- kcl-ezpz/benches/solver_bench.rs
+++ kcl-ezpz/benches/solver_bench.rs
@@ -182,42 +182,6 @@ fn solve_two_rectangles_dependent(c: &mut Criterion) {
     });
 }
 
-fn solve_angled_lines(c: &mut Criterion) {
-    let mut id_generator = IdGenerator::default();
-    // It has 4 points.
-    let p0 = DatumPoint::new(&mut id_generator);
-    let p1 = DatumPoint::new(&mut id_generator);
-    let p2 = DatumPoint::new(&mut id_generator);
-    let line0 = LineSegment::new(p0, p1);
-    let line1 = LineSegment::new(p1, p2);
-    let constraints = vec![
-        // p0 is the origin
-        Constraint::Fixed(p0.id_x(), 0.0),
-        Constraint::Fixed(p0.id_y(), 0.0),
-        // Both lines are parallel
-        Constraint::lines_parallel([line0, line1]),
-        // Both lines are the same distance
-        Constraint::Distance(p0, p1, 32.0f64.sqrt()),
-        Constraint::Distance(p1, p2, 32.0f64.sqrt()),
-        Constraint::Fixed(p1.id_x(), 4.0),
-    ];
-
-    let initial_guesses = vec![
-        (p0.id_x(), 0.0),
-        (p0.id_y(), 0.0),
-        (p1.id_x(), 3.0f64.sqrt()),
-        (p1.id_y(), 3.0f64.sqrt()),
-        (p2.id_x(), 6.0f64.sqrt()),
-        (p2.id_y(), 6.0f64.sqrt()),
-    ];
-
-    c.bench_function("solve angled lines", |b| {
-        b.iter(|| {
-            let _actual = black_box(solve(constraints.clone(), initial_guesses.clone()).unwrap());
-        })
-    });
-}
-
 fn solve_massive(c: &mut Criterion) {
     let mut group = c.benchmark_group("massively_parallel");
     for size in [1, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500].iter() {
@@ -248,7 +212,6 @@ criterion_group!(
     solve_easy,
     solve_two_rectangles,
     solve_two_rectangles_dependent,
-    solve_angled_lines,
     solve_massive,
 );
 criterion_main!(benches);
