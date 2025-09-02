7591cdc88aefac89169e36a7d3e4049c2e67b154
Adam Chalmers <adam.chalmers@zoo.dev>
2025-09-03 10:16:22 +1200

    Neaten up the Python script (#51)
    Make the guess-generation consistent with the point-generation
    Put it all under the `if __name__ == "__main__"` block

--- test_cases/massive_parallel_system/gen_big_problem.py
+++ test_cases/massive_parallel_system/gen_big_problem.py
@@ -9,25 +9,27 @@ def get_overconstrain():
         return False
 
 
-total_lines = int(sys.argv[1])
-overconstrain = get_overconstrain()
+if __name__ == "__main__":
+    total_lines = int(sys.argv[1])
+    overconstrain = get_overconstrain()
 
-print("# constraints")
-for line in range(total_lines):
-    a = line * 2
-    b = line * 2 + 1
-    print(f"point p{a}")
-    print(f"point p{b}")
-    print(f"vertical(p{a}, p{b})")
-    print(f"p{a}.x={line}")
-    print(f"p{a}.y=0")
-    print(f"p{b}.y=4")
-    if overconstrain:
-        print(f"distance(p{a}, p{b}, 4)")
+    print("# constraints")
+    for line in range(total_lines):
+        a = line * 2
+        b = line * 2 + 1
+        print(f"point p{a}")
+        print(f"point p{b}")
+        print(f"vertical(p{a}, p{b})")
+        print(f"p{a}.x={line}")
+        print(f"p{a}.y=0")
+        print(f"p{b}.y=4")
+        if overconstrain:
+            print(f"distance(p{a}, p{b}, 4)")
 
-print()
-print("# guesses")
-for line in range(total_lines):
-    start = line * 2
-    print(f"p{start} roughly ({start},{start})")
-    print(f"p{start+1} roughly ({start+1},{start+1})")
+    print()
+    print("# guesses")
+    for line in range(total_lines):
+        a = line * 2
+        b = line * 2 + 1
+        print(f"p{a} roughly ({a},{a})")
+        print(f"p{b} roughly ({b},{b})")
