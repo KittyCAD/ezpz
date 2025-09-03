edcf9b14e19a7ae62db07555b226a7a82c1091bd
Adam Chalmers <adam.chalmers@zoo.dev>
2025-09-03 13:57:52 -0500

    Parser: introduce 'inside_brackets' combinator

--- kcl-ezpz/src/textual/parser.rs
+++ kcl-ezpz/src/textual/parser.rs
@@ -9,6 +9,7 @@ use winnow::{
     ModalResult as WResult,
     ascii::{alphanumeric1, digit1, newline, space0},
     combinator::{alt, delimited, opt, separated},
+    error::{ContextError, ErrMode},
     prelude::*,
 };
 
@@ -65,44 +66,21 @@ pub fn parse_declare_point(i: &mut &str) -> WResult<DeclarePoint> {
 pub fn parse_horizontal(i: &mut &str) -> WResult<Horizontal> {
     let _ = "horizontal".parse_next(i)?;
     ignore_ws(i);
-    let _ = '('.parse_next(i)?;
-    ignore_ws(i);
-    let p0 = parse_label(i)?;
-    let _ = ','.parse_next(i)?;
-    ignore_ws(i);
-    let p1 = parse_label(i)?;
-    let _ = ')'.parse_next(i)?;
+    let [p0, p1] = inside_brackets(two_points, i)?;
     Ok(Horizontal { label: (p0, p1) })
 }
 
 pub fn parse_vertical(i: &mut &str) -> WResult<Vertical> {
     let _ = "vertical".parse_next(i)?;
     ignore_ws(i);
-    let _ = '('.parse_next(i)?;
-    ignore_ws(i);
-    let p0 = parse_label(i)?;
-    let _ = ','.parse_next(i)?;
-    ignore_ws(i);
-    let p1 = parse_label(i)?;
-    let _ = ')'.parse_next(i)?;
+    let [p0, p1] = inside_brackets(two_points, i)?;
     Ok(Vertical { label: (p0, p1) })
 }
 
 pub fn parse_distance(i: &mut &str) -> WResult<Distance> {
     let _ = "distance".parse_next(i)?;
     ignore_ws(i);
-    let _ = '('.parse_next(i)?;
-    ignore_ws(i);
-    let p0 = parse_label(i)?;
-    let _ = ','.parse_next(i)?;
-    ignore_ws(i);
-    let p1 = parse_label(i)?;
-    ignore_ws(i);
-    let _ = ','.parse_next(i)?;
-    ignore_ws(i);
-    let distance = parse_number_expr(i)?;
-    ignore_ws(i);
-    let _ = ')'.parse_next(i)?;
+    let ([p0, p1], _, distance) = inside_brackets((two_points, commasep, parse_number_expr), i)?;
     Ok(Distance {
         label: (p0, p1),
         distance,
@@ -119,13 +97,7 @@ pub fn commasep(i: &mut &str) -> WResult<()> {
 pub fn parse_angle_line(i: &mut &str) -> WResult<AngleLine> {
     let _ = "lines_at_angle".parse_next(i)?;
     ignore_ws(i);
-    let _ = '('.parse_next(i)?;
-    ignore_ws(i);
-    let [p0, p1, p2, p3] = four_points(i)?;
-    commasep(i)?;
-    let angle = parse_angle(i)?;
-    ignore_ws(i);
-    let _ = ')'.parse_next(i)?;
+    let ([p0, p1, p2, p3], _, angle) = inside_brackets((four_points, commasep, parse_angle), i)?;
     let line0 = (p0, p1);
     let line1 = (p2, p3);
     Ok(AngleLine {
@@ -148,10 +120,7 @@ pub fn parse_angle(i: &mut &str) -> WResult<Angle> {
 pub fn parse_parallel(i: &mut &str) -> WResult<Parallel> {
     let _ = "parallel".parse_next(i)?;
     ignore_ws(i);
-    let _ = '('.parse_next(i)?;
-    ignore_ws(i);
-    let [p0, p1, p2, p3] = four_points(i)?;
-    let _ = ')'.parse_next(i)?;
+    let [p0, p1, p2, p3] = inside_brackets(four_points, i)?;
     let line0 = (p0, p1);
     let line1 = (p2, p3);
     Ok(Parallel { line0, line1 })
@@ -160,13 +129,22 @@ pub fn parse_parallel(i: &mut &str) -> WResult<Parallel> {
 pub fn parse_perpendicular(i: &mut &str) -> WResult<Perpendicular> {
     let _ = "perpendicular".parse_next(i)?;
     ignore_ws(i);
-    let _ = '('.parse_next(i)?;
-    ignore_ws(i);
-    let [p0, p1, p2, p3] = four_points(i)?;
-    let _ = ')'.parse_next(i)?;
+    let [p0, p1, p2, p3] = inside_brackets(four_points, i)?;
     let line0 = (p0, p1);
     let line1 = (p2, p3);
     Ok(Perpendicular { line0, line1 })
+}
+
+/// Runs the given parser, surrounded by parentheses.
+fn inside_brackets<'i, T>(
+    mut parser: impl Parser<&'i str, T, ErrMode<ContextError>>,
+    i: &mut &'i str,
+) -> WResult<T> {
+    let _ = '('.parse_next(i)?;
+    ignore_ws(i);
+    let t = parser.parse_next(i)?;
+    let _ = ')'.parse_next(i)?;
+    Ok(t)
 }
 
 fn four_points(i: &mut &str) -> WResult<[Label; 4]> {
@@ -179,6 +157,14 @@ fn four_points(i: &mut &str) -> WResult<[Label; 4]> {
     let p3 = parse_label(i)?;
     ignore_ws(i);
     Ok([p0, p1, p2, p3])
+}
+
+fn two_points(i: &mut &str) -> WResult<[Label; 2]> {
+    let p0 = parse_label(i)?;
+    commasep(i)?;
+    let p1 = parse_label(i)?;
+    ignore_ws(i);
+    Ok([p0, p1])
 }
 
 /// Single-element vector
@@ -262,12 +248,10 @@ fn parse_label(i: &mut &str) -> WResult<Label> {
 }
 
 pub fn parse_point(input: &mut &str) -> WResult<Point> {
-    delimited(
-        '(',
+    inside_brackets(
         (parse_number, ',', space0, parse_number).map(|(x, _comma, _space, y)| Point { x, y }),
-        ')',
+        input,
     )
-    .parse_next(input)
 }
 
 fn parse_number(i: &mut &str) -> WResult<f64> {
