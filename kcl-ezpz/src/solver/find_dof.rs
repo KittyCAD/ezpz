51a172ec432b9b09bb561fbd8b309b9f0b8ee393
Adam Chalmers <adam.chalmers@zoo.dev>
2025-12-25 12:42:20 -0600

    Break the find-DOF code into two parts
    Separate the SVD decomposition from using the SVD
    decomposition.

--- kcl-ezpz/src/solver/find_dof.rs
+++ kcl-ezpz/src/solver/find_dof.rs
@@ -21,58 +21,68 @@ impl Model<'_> {
 
         // SVD decomposes `J` into `J = UΣVᵀ`.
         let svd = j_dense.svd().map_err(NonLinearSystemError::FaerSvd)?;
-        let sigma_diags = svd.S();
+        let svd_s = svd.S();
+        let svd_v = svd.V();
 
-        // These are the 'singular values'.
-        let sigma_col = sigma_diags.column_vector();
-
-        // The system is underconstrained if there's too many singular values
-        // close to 0. How close to 0? The tolerance should be derived from
-        // the largest singular value.
-        let largest_singular_value = sigma_col
-            .iter()
-            .copied()
-            .reduce(libm::fmax)
-            .ok_or(NonLinearSystemError::EmptySystemNotAllowed)?;
-        let tolerance = 1e-8 * largest_singular_value;
-
-        let rank = sigma_col.iter().filter(|&&s| s > tolerance).count();
-
-        // The degrees of freedom = nvars - rank;
-        // The rank is a measure of how sensitive the Jacobian is in each direction.
-        // If there's any direction where the Jacobian is sensitive, then tweaking the values
-        // in that dimension will change the result. This is what we'd expect in a well-constrained system.
-        // On the other hand, if the Jacobian DOESN'T change along one direction, that implies the direction
-        // doesn't affect the residual at all. That's basically exactly what a degree of freedom means.
-
-        // Nullspace column indices in V, as in J = U.sigma.V in the SVD decomposition.
-        let degrees_of_freedom: Vec<usize> = (rank..nvars).collect();
-
-        // Compute participation norm for each variable.
-        // If a variable's participation is basically zero, then it's constrained.
-        // If it's nonzero, then it moves in some DOF and is unconstrained.
-        let participation: Vec<_> = (0..nvars)
-            .map(|j| {
-                let mut sum_sq = 0.0;
-
-                for &k in &degrees_of_freedom {
-                    // V[j, k] is the component of variable j for the k-th DOF.
-                    let v_jk = svd.V().get(j, k);
-                    sum_sq += v_jk * v_jk;
-                }
-                sum_sq.sqrt()
-            })
-            .collect();
-        let max_participation = participation.iter().copied().fold(0.0, libm::fmax);
-
-        // Relative threshold to classify variables
-        let var_tol = 1e-3 * max_participation;
-
-        let underconstrained: Vec<crate::Id> = (0..nvars)
-            .filter(|&j| participation[j] > var_tol)
-            .map(|x| x as u32)
-            .collect();
-
+        let underconstrained = calculate(svd_s, svd_v, nvars)?;
         Ok(FreedomAnalysis::new(underconstrained))
     }
+}
+
+fn calculate(
+    svd_sigma: faer::diag::generic::Diag<faer::diag::Ref<'_, f64>>,
+    svd_v: faer::mat::generic::Mat<faer::mat::Ref<'_, f64>>,
+    nvars: usize,
+) -> Result<Vec<crate::Id>, NonLinearSystemError> {
+    // These are the 'singular values'.
+    let sigma_col = svd_sigma.column_vector();
+
+    // The system is underconstrained if there's too many singular values
+    // close to 0. How close to 0? The tolerance should be derived from
+    // the largest singular value.
+    let largest_singular_value = sigma_col
+        .iter()
+        .copied()
+        .reduce(libm::fmax)
+        .ok_or(NonLinearSystemError::EmptySystemNotAllowed)?;
+    let tolerance = 1e-8 * largest_singular_value;
+
+    let rank = sigma_col.iter().filter(|&&s| s > tolerance).count();
+
+    // The degrees of freedom = nvars - rank;
+    // The rank is a measure of how sensitive the Jacobian is in each direction.
+    // If there's any direction where the Jacobian is sensitive, then tweaking the values
+    // in that dimension will change the result. This is what we'd expect in a well-constrained system.
+    // On the other hand, if the Jacobian DOESN'T change along one direction, that implies the direction
+    // doesn't affect the residual at all. That's basically exactly what a degree of freedom means.
+
+    // Nullspace column indices in V, as in J = U.sigma.V in the SVD decomposition.
+    let degrees_of_freedom: Vec<usize> = (rank..nvars).collect();
+
+    // Compute participation norm for each variable.
+    // If a variable's participation is basically zero, then it's constrained.
+    // If it's nonzero, then it moves in some DOF and is unconstrained.
+    let participation: Vec<_> = (0..nvars)
+        .map(|j| {
+            let mut sum_sq = 0.0f64;
+
+            for &k in &degrees_of_freedom {
+                // V[j, k] is the component of variable j for the k-th DOF.
+                let v_jk = svd_v.get(j, k);
+                sum_sq += v_jk * v_jk;
+            }
+            sum_sq.sqrt()
+        })
+        .collect();
+    let max_participation = participation.iter().copied().fold(0.0, libm::fmax);
+
+    // Relative threshold to classify variables
+    let var_tol = 1e-3 * max_participation;
+
+    let underconstrained = (0..nvars)
+        .filter(|&j| participation[j] > var_tol)
+        .map(|x| x as u32)
+        .collect();
+
+    Ok(underconstrained)
 }
